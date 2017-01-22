a45d3e757a2f2a7db3826a28ee9151dbe7655584
avandeloo <anthony.vandeloo@gmail.com>
2017-01-21 23:08:50 -0600

    removed/corrected redundant code

--- algo/palindrome.rb
+++ algo/palindrome.rb
@@ -1,7 +1,7 @@
 # Complete the method called palindrome? which should accept a string as a parameter and return a boolean indicated whether the string is a palindrome. A palindrome is a word that reads the same both forwards and backwards. Examples: eye, madam, racecar
 
 def palindrome?(string)
-  string = string.downcase.tr("^a-z", "").downcase
+  string = string.downcase.tr("^a-z", "")
   if string == string.reverse
     return true
   else
